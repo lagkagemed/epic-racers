3fd20ab8ead8f83006b7da0fe8d1d651ddac8fbd
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2021-09-18 16:35:55 +0100

    socket setup

--- app.js
+++ app.js
@@ -19,4 +19,28 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(process.env.PORT || 2000);
 console.log('Server started.');
 
+let SOCKET_LIST = {}
+let PLAYER_LIST = {}
+
 let io = new Server(serv);
+
+io.sockets.on('connection', function(socket){
+    socket.id = Math.random();
+
+    let player = {}
+    player.x = 100
+    player.y = 100
+    player.dir = 0
+    player.id = socket.id
+
+    SOCKET_LIST[socket.id] = socket;
+    PLAYER_LIST[player.id] = player;
+
+    console.log('Socket connected!');
+
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[player.id];
+        console.log('Socket disconnected');
+    });
+});
