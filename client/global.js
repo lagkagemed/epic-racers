de8049674f872d8bb3380e9a006ae205bd31273e
birkb85 <birkelskerlinette@gmail.com>
2021-09-18 17:39:30 +0200

    Test draw car.

--- client/global.js
+++ client/global.js
@@ -2,3 +2,13 @@ let canvas = document.getElementById("ctx")
 let ctx = canvas.getContext("2d")
 ctx.imageSmoothingEnabled = false
 ctx.webkitImageSmoothingEnabled = false
+
+
+
+
+// // Testing draw car
+// var img1 = new Image();
+// img1.onload = function () {
+//     ctx.drawImage(img1, 16, 16);
+// };
+// img1.src = './client/assets/car_blue.png';
