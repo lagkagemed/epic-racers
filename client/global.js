54d6bcabb470b7704a129677d1754eaec39ff14a
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2021-09-18 16:30:21 +0100

    error fixed

--- client/global.js
+++ client/global.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById("ctx")
+let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
 ctx.imageSmoothingEnabled = false
 ctx.webkitImageSmoothingEnabled = false
