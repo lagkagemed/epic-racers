df38fadff459a3298dd72e3bb8964d36d8c58e63
birkb85 <birkelskerlinette@gmail.com>
2021-09-18 18:53:21 +0200

    Testing rotating cars

--- client/index.js
+++ client/index.js
@@ -42,17 +42,27 @@ function sendInfo() {
 function updateSillyMovement() {
     for (let i in PLAYER_LIST) {
         let player = PLAYER_LIST[i]
-        if (player.pressingUp) player.y--
-        if (player.pressingDown) player.y++
-        if (player.pressingLeft) player.x--
-        if (player.pressingRight) player.x++
+        if (player.pressingUp) player.y--;
+        if (player.pressingDown) player.y++;
+        if (player.pressingLeft) 
+        {
+            player.dir -= 0.1;
+        }
+        if (player.pressingRight)
+        {
+            player.dir += 0.1;
+        }
     }
 }
 
 function drawCars() {
     for (let i in PLAYER_LIST) {
         let player = PLAYER_LIST[i]
-        ctx.drawImage(carBlueSpr, player.x, player.y);
+        ctx.translate(player.x, player.y);
+        ctx.rotate(player.dir); //angleInRadians
+        ctx.drawImage(carBlueSpr, -carBlueSpr.width / 2, -carBlueSpr.height / 2);
+        ctx.rotate(-player.dir); //angleInRadians
+        ctx.translate(-player.x, -player.y);
     }
 }
 
