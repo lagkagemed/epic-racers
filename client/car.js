8f5cd131fb571200dd744c06f5a9907999530082
birkb85 <birkelskerlinette@gmail.com>
2021-09-18 19:25:47 +0200

    testing car movement

--- client/car.js
+++ client/car.js
@@ -1,18 +1,42 @@
+const acc = 0.1;
+const accMax = 1;
+const speedMax = 3;
+
 function carUpdate(player)
 {
-    if (player.pressingUp) player.y--;
+    if (player.pressingUp)
+    {
+        player.acc += acc;
+        if (player.acc > accMax) player.acc = accMax;
 
-    if (player.pressingDown) player.y++;
+        player.speed += player.acc;
+        if (player.speed > speedMax) player.speed = speedMax;
+    }
+    else if (player.pressingDown)
+    {
+        player.acc -= acc;
+        if (player.acc < -accMax) player.acc = -accMax;
+
+        player.speed += player.acc;
+        if (player.speed < -speedMax) player.speed = -speedMax;
+    }
+    // else
+    // {
+    //     if ()
+    // }
 
     if (player.pressingLeft) 
     {
         player.dir -= 0.1;
     }
-    
+
     if (player.pressingRight)
     {
         player.dir += 0.1;
     }
+
+    // player.x = 
+    player.y += player.speed;
 }
 
 function carDraw(player)
