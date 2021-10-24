c282b831848a974abca87c4f7228c74a9621ea88
Kennet Jonas Bonnicksen <kennet_jonas@hotmail.com>
2021-10-24 21:51:04 +0100

    beginning of box.cjs

---
+++ client/box.cjs
@@ -0,0 +1,14 @@
+(function(exports){
+
+    exports.new = function() {
+         let self = {}
+         return self
+    }
+
+    exports.update = function(){
+    };
+
+    exports.draw = function() {
+    }
+ 
+ }(typeof exports === 'undefined' ? this.Box = {} : exports));
